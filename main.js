94d9e89eb0899418990d2d8ff15a912ff4ab1fe9
jotadgame <131823048+jotadgame@users.noreply.github.com>
2023-08-31 18:24:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Crie a variável canvas
-  var canvas = new fabric.canvas('myCanvas')
+  var canvas = new fabric.Canvas('myCanvas')
 
 block_y=1;
 block_x=1;
@@ -14,8 +14,8 @@ function new_image(get_image)
 {
 	fabric.Image.fromURL(get_image, function(Img) { 
 	block_image_object = Img;
-	block_image_object.scaleToWidth(block_image_width)
-	block_image_object.scaleToHeight(block_image_Height)
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
 	block_image_object.set({top:block_y,left:block_x});	
 	canvas.add(block_image_object)
 	})
